11f2034271edfb6da89f8c670df283b0108eaee3
DagWuan <Khoa0392631243@gmail.com>
2025-02-11 18:49:49 +0700

    Update quan.py

--- quan.py
+++ quan.py
@@ -244,7 +244,7 @@ if chon == '8.1' :
 if chon == '8.2' :
      exec(requests.get('https://raw.githubusercontent.com/Khanh23047/Reg-pro5-vip/main/reg.py').text)
 if chon == '8.3' :
-    exec(requests.get('https://raw.githubusercontent.com/DagWuan/DagWuan/refs/heads/main/Tools1.py').text)
+    exec(requests.get('https://raw.githubusercontent.com/DagWuan/DagWuan/refs/heads/main/Tools2.py').text)
 if chon == '8.4' :
     exec(requests.get('https://raw.githubusercontent.com/DagWuan/DagWuan/refs/heads/main/Phanhoi.py').text)
 if chon == '8.5' :
