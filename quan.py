bba18672ae56c9bba8abeae58e47d767232b3733
DagWuan <Khoa0392631243@gmail.com>
2025-03-25 21:49:34 +0700

    Update quan.py

--- quan.py
+++ quan.py
@@ -61,6 +61,7 @@ def show_key_info(key):
 """)
     
     print(f"{do}Tool đang update dự kiến sẽ là {trang}ngày 1-4-2025")
+    print(f"{luc}Hãy lên YTb: DagWuan Share Tools để được cập nhật tools mới!!
 
 # Main function
 def main():
